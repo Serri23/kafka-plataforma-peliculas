c6c154009fcec1a56014da001b1f9402eae39f05
HIBERUS\aserrano <aserranoa@hiberus.com>
2023-10-09 18:00:21 +0200

    [Aplicador valoraciones] - Creacion de avros y configuracion

--- aplicador-valoraciones/src/main/java/com/hiberus/aplicadorvaloraciones/AplicadorValoracionesApplication.java
+++ aplicador-valoraciones/src/main/java/com/hiberus/aplicadorvaloraciones/AplicadorValoracionesApplication.java
@@ -1,4 +1,4 @@
-package com.hiberus.consultadorpeliculas;
+package com.hiberus.aplicadorvaloraciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
